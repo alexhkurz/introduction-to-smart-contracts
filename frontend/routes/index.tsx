88bd9e8b4e670f2f20e8c38d414f9a086c3fd633
Stephen Preston <stevepreston747@gmail.com>
2023-05-04 18:01:13 -0700

    add config file

--- frontend/routes/index.tsx
+++ frontend/routes/index.tsx
@@ -5,6 +5,7 @@ import { useCallback, useEffect, useReducer, useState } from 'react';
 import { load } from 'std/dotenv/mod.ts';
 import Form from '~/components/Form.tsx';
 import { ethers } from 'ethers';
+import { config } from '~/sponsorship.ts';
 
 // const env = await load();
 // const ETHERSCAN_API_KEY = env['ETHERSCAN_API_KEY'];
@@ -143,87 +144,87 @@ export default function Index() {
       </div>
 
       <div className="flex flex-col items-center mb-12 py-4 mx-20 rounded-xl">
-        <div className="pb-10">
-          {web3Provider ? (
-            <>
-              <h1 className="text-3xl text-center mt-4 pt-10">
-                {' '}
-                Connected Wallet Address: {address}
-              </h1>
-              <div className="pt-20">
-                <button
-                  className={
-                    'text-lg px-3 py-2 rounded-lg text-white dark:text-black bg-[#0095D4] dark:bg-[#0095D4]'
-                  }
-                  onClick={disconnect}
-                >
-                  Disconnect
-                </button>
-              </div>
-            </>
-          ) : (
-            <div className="flex flex-col items-center pt-12">
+        {web3Provider ? (
+          <>
+            <h1 className="text-3xl text-center mt-4 pt-10">
+              {' '}
+              Connected Wallet Address: {address}
+            </h1>
+            <div className="py-10">
               <button
                 className={
                   'text-lg px-3 py-2 rounded-lg text-white dark:text-black bg-[#0095D4] dark:bg-[#0095D4]'
                 }
-                onClick={connect}
+                onClick={disconnect}
               >
-                Connect Wallet
+                Disconnect
               </button>
             </div>
-          )}
-        </div>
 
-        <div className="flex flex-row items-center space-x-4 bg-gray-700 rounded-xl  p-4">
-          <Form
-            setValue={setSendAmount}
-            className="bg-gray-200 border-none rounded-md outline-none focus:ring-0 text-3xl"
-            placeholder="0.001"
-          />
-          <button
-            className={
-              'text-md px-3 py-2 w-auto rounded-lg text-white bg-[#0095D4]'
-            }
-            onClick={async () => {
-              console.log(sendAmount);
-              if (web3Provider) {
-                const abi = ['function deposit() external payable'];
-
-                const treasuryAddress =
-                  '0xb2550a22fc57397475fD0B0A7487702B603dA0BA';
-                const signer = web3Provider.getSigner();
-                const treasuryContract = new ethers.Contract(
-                  treasuryAddress,
-                  abi,
-                  signer
-                );
-
-                try {
-                  const transactionResponse = await treasuryContract.deposit({
-                    gasPrice: await web3Provider.getGasPrice(),
-                    gasLimit: 140000,
-                    value: ethers.utils.parseEther(sendAmount),
-                  });
-
-                  // console.log(transactionResponse.hash);
-                  setTxHash(transactionResponse.hash);
-
-                  const transactionReceipt = await transactionResponse.wait();
-                  // console.log(transactionReceipt.logs);
-                  if (transactionReceipt.status === 1) {
-                    // transaction.success = true;
-                  }
-                } catch (error: any) {
-                  console.log(error);
+            <div className="flex flex-row items-center space-x-4 bg-gray-700 rounded-xl  p-4">
+              <Form
+                setValue={setSendAmount}
+                className="bg-gray-200 border-none rounded-md outline-none focus:ring-0 text-3xl"
+                placeholder="0.001"
+              />
+              <button
+                className={
+                  'text-md px-3 py-2 w-auto rounded-lg text-white bg-[#0095D4]'
                 }
+                onClick={async () => {
+                  console.log(sendAmount);
+                  if (web3Provider) {
+                    const abi = ['function deposit() external payable'];
+
+                    const treasuryAddress = config.goerli_contract_address;
+                    const signer = web3Provider.getSigner();
+                    const treasuryContract = new ethers.Contract(
+                      treasuryAddress,
+                      abi,
+                      signer
+                    );
+
+                    try {
+                      const transactionResponse =
+                        await treasuryContract.deposit({
+                          gasPrice: await web3Provider.getGasPrice(),
+                          gasLimit: 140000,
+                          value: ethers.utils.parseEther(sendAmount),
+                        });
+
+                      // console.log(transactionResponse.hash);
+                      setTxHash(transactionResponse.hash);
+
+                      const transactionReceipt =
+                        await transactionResponse.wait();
+                      // console.log(transactionReceipt.logs);
+                      if (transactionReceipt.status === 1) {
+                        // transaction.success = true;
+                      }
+                    } catch (error: any) {
+                      console.log(error);
+                    }
+                  }
+                }}
+              >
+                Send
+              </button>
+            </div>
+            <p className="text-xl pt-4">In ETH</p>
+          </>
+        ) : (
+          <div className="flex flex-col items-center py-12">
+            <button
+              className={
+                'text-lg px-3 py-2 rounded-lg text-white dark:text-black bg-[#0095D4] dark:bg-[#0095D4]'
               }
-            }}
-          >
-            Send
-          </button>
-        </div>
-        <p className="text-xl pt-4">In ETH</p>
+              onClick={connect}
+            >
+              Connect Wallet
+            </button>
+          </div>
+        )}
+
         {txHash ? (
           <>
             <p className="pt-5">Transaction Hash: {txHash}</p>
